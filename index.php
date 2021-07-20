ca4eab3a373d1abe2fbfb222b63a6ff2db5487be
madhurabhokare <46599975+madhurabhokare@users.noreply.github.com>
2021-07-20 17:50:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+<!Document html>
+<head>
+    <title>WT_Assignment1</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <<link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ 
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">ManuRooks</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      
+       <li class="nav-item">
+           <a class="nav-link" href="about.php">About</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li><!-- comment -->
+          
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="images/b2.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/b3.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/b4.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+    
+    <section class ="my-5"> 
+        <div class="py-5">
+        <h2 class="text-center">About Us</h2>
+    </div>
+<div class="container-fluid">
+    <div class ="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/b1.jpg" class ="img-fluid aboutimg">
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+            <h3 class ="display-4">Home made cake Shop</h3><br>
+            <p class="py-1">Starting a cake business from home is already a tough decision. The next step is to get out, advertise and market your cake business. Finding customers can sometimes be difficult and finding the right customers can be even harder. Here are some simple, easy ways you can use to let people know how to find your business.
+            </p>
+            <a href ="about.php" class="btn btn-success">Check More</a>
+        </div>
+        
+    </div>
+</div>
+</section>
+    
+<section class ="my-5"> 
+        <div class="py-5">
+        <h2 class="text-center">Our Services</h2>
+    </div>
+    <div class ="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card" >
+                    <img class="card-img-top" src="images/b3.jpg" alt="Card image">
+         <div class="card-body">
+           <h4 class="card-title">Black Forest</h4>
+           <p class="card-text">Fresh & home made cake.</p>
+           <a href="#" class="btn btn-primary">See Profile</a>
+         </div>
+    </div>
+                
+            </div>
+             <div class="col-lg-4 col-md-4 col-12">
+                <div class="card" >
+                    <img class="card-img-top" src="images/b3.jpg" alt="Card image">
+         <div class="card-body">
+           <h4 class="card-title">Black Forest</h4>
+           <p class="card-text">Fresh & home made cake.</p>
+           <a href="#" class="btn btn-primary">See Profile</a>
+         </div>
+    </div>
+                
+            </div>
+             <div class="col-lg-4 col-md-4 col-12">
+                <div class="card" >
+                    <img class="card-img-top" src="images/b2.jpg" alt="Card image">
+         <div class="card-body">
+           <h4 class="card-title">Black Forest</h4>
+           <p class="card-text">Fresh & home made cake.</p>
+           <a href="#" class="btn btn-primary">See Profile</a>
+         </div>
+    </div>
+                
+            </div>
+        </div>
+    </div>
+    
+</section>
+ 
+    <section class ="my-5"> 
+        <div class="py-5">
+        <h2 class="text-center">Gallary</h2>
+       </div>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg4 col-md-4 col-12">
+                    <img src="images/gallery-img1.jpg" class="img-fluid pb-3">
+                </div>
+                 <div class="col-lg4 col-md-4 col-12">
+                    <img src="images/gallery-img1.jpg" class="img-fluid pb-3">
+                 </div>
+                 <div class="col-lg4 col-md-4 col-12">
+                    <img src="images/gallery-img1.jpg" class="img-fluid pb-3">
+                 </div>
+                 <div class="col-lg4 col-md-4 col-12">
+                    <img src="images/gallery-img1.jpg" class="img-fluid pb-3">
+                </div>
+                 <div class="col-lg4 col-md-4 col-12">
+                    <img src="images/gallery-img1.jpg" class="img-fluid pb-3">
+                </div>
+                 <div class="col-lg4 col-md-4 col-12">
+                    <img src="images/gallery-img1.jpg" class="img-fluid pb-3">
+                </div>
+            </div>
+                
+        </div>
+        
+    </section>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
